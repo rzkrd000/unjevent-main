7991fbce564b84f47945140e6753749633422015
rzkrd000 <rizkard08@gmail.com>
2021-12-10 23:02:39 +0700

    commit on 'master'

--- js/app.js
+++ js/app.js
@@ -9,35 +9,6 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-// Pricing
-var headerMonthly = document.getElementsByClassName("header-monthly")
-var headerAnnual = document.getElementsByClassName("header-annual")
-var priceMonthly = document.getElementsByClassName("price-monthly")
-var priceAnnual = document.getElementsByClassName("price-annual")
-var durationMonth = document.getElementsByClassName("duration-month")
-var durationYear = document.getElementsByClassName("duration-year")
-
-function checker(checked) {
-  checked ? showAnnual() : showMonthly()
-}
-
-function showAnnual() {
-  headerMonthly[0].style.display = "none"
-  headerAnnual[0].style.display = "inline-block"
-  priceMonthly[0].style.display = "none"
-  priceAnnual[0].style.display = "inline-block"
-  durationMonth[0].style.display = "none"
-  durationYear[0].style.display = "inline-block"
-}
-
-function showMonthly() {
-  headerMonthly[0].style.display = "inline-block"
-  headerAnnual[0].style.display = "none"
-  priceMonthly[0].style.display = "inline-block"
-  priceAnnual[0].style.display = "none"
-  durationMonth[0].style.display = "inline-block"
-  durationYear[0].style.display = "none"
-}
 
 //slider js
 $(document).ready(function() {
